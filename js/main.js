927e923cf29b41d52b7e809dadf7b9470af064a4
Jozef <jozefw@sbcglobal.net>
2015-06-06 18:04:04 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+$(function(){
+
+	new CurrentTimeView({
+		el: document.getElementById("current-time")
+	});
+});
+
+// give your data a name
+var newDataFlag = 'Data-Arrived';
+var Dispatcher = _.extend({},Backbone.Events);
+
+setInterval(function(){
+	var currentTime = (new Date()).toLocaleTimeString();
+	Dispatcher.trigger(newDataFlag, currentTime);
+},1000);
+
+
+var CurrentTimeView = Backbone.View.extend({
+	initialize: function(){
+		// bind "render" to this view with Underscores "bindAll"
+		_(this).bindAll('render');
+		// render this view when new data is broadcast
+		Dispatcher.on(newDataFlag, this.render);
+	},
+
+		render: function(currentTime){
+			// set the DOMS elements text content to the clocks current time
+			this.el.textContent = currentTime;
+		}
+});
+
+
